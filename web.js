0034ef6296759c10357644c4b9001b4518d116c3
sukhmeet <sukhmeet2390@gmail.com>
2013-07-29 14:34:49 +0530

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var data = fs.readFileSync('index.html');
+	response.send(new Buffer(data));		
+  
 });
 
 var port = process.env.PORT || 5000;
